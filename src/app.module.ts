07ae09ab489e98deecd71fc97573aa5d9af78434
Vladislav <vladozdeveloper@gmail.com>
2022-10-17 23:59:46 +0300

    created a new comment, auth, video resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { CommentModule } from './comment/comment.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, VideoModule, CommentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
