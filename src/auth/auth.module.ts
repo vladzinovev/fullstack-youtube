8788eb72d2ce6ee5c3eeb0cedffdb5aa826f2d38
Vladislav <vladozdeveloper@gmail.com>
2022-10-18 19:04:32 +0300

    set up a JWT strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModel } from 'src/user/user.model';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'src/config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports:[
     TypegooseModule.forFeature([
       {
@@ -17,7 +20,12 @@ import { ConfigModule } from '@nestjs/config';
         }
       }
     ]),
-    ConfigModule
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:getJWTConfig,
+    })
   ]
   
 })
