1e959e92f94bf61107771ef9c6cf8337ed4769ec
Vladislav <vladozdeveloper@gmail.com>
2022-10-19 15:00:18 +0300

    installed the required libraries and created folders for the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
