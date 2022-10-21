cbd700217785b04b30bd726611953b58708ee560
Vladislav <vladozdeveloper@gmail.com>
2022-10-21 23:56:57 +0300

    fixed bugs

--- frontend/app/components/layout/header/auth-form/AuthForm.tsx
+++ frontend/app/components/layout/header/auth-form/AuthForm.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { BsPlusCircleFill } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
+import Input from "../../../ui/input/Input";
 import styles from './AuthForm.module.scss';
 
 const AuthForm:FC=()=>{
@@ -10,7 +11,7 @@ const AuthForm:FC=()=>{
                 <FaUserCircle fill='#A4A4A4'/>
             </button>
             <div className={styles.form}>
-                
+                <Input/>
             </div>
 
         </div>
