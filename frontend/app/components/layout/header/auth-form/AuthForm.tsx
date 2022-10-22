6a472cd7524e133ecb309e785f66969f630b5cb4
Vladislav <vladozdeveloper@gmail.com>
2022-10-22 23:37:24 +0300

    added a field email and password

--- frontend/app/components/layout/header/auth-form/AuthForm.tsx
+++ frontend/app/components/layout/header/auth-form/AuthForm.tsx
@@ -1,5 +1,5 @@
 import Button from "@/components/ui/Button/Button";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FaUserCircle } from "react-icons/fa";
 import Field from "../../../ui/Field/Field";
@@ -9,13 +9,16 @@ import styles from './AuthForm.module.scss';
 
 const AuthForm:FC=()=>{
 
+    const [type,setType]=useState<'login'|'register'>('login');
+
     const {register, formState:{errors},handleSubmit}=useForm<IAuthFields>({
         //чтобы срабатывала проверка при пустом поле
         mode: 'onChange'
     });
 
     const onSubmit:SubmitHandler<IAuthFields>=(data)=>{
-        alert(data.email)
+        if(type==='login') console.log('login',data.email)
+        else if (type==='register') console.log('register',data.email)
     }
 
     return (
@@ -34,8 +37,18 @@ const AuthForm:FC=()=>{
                         })
                     }
                 placeholder='Email' error={errors.email}/>
-                <Button>Login</Button>
-                <button className={'text-sm'}>Register</button>
+                <Field 
+                    {...register('password',{
+                            required:'Password is required',
+                            minLength:{
+                                value:6,
+                                message:'Please enter a valid password address',
+                            },
+                        })
+                    }
+                placeholder='Password' error={errors.password}/>
+                <Button type='submit' onClick={()=>setType('login')}>Login</Button>
+                <button className={'text-sm'} onClick={()=>setType('register')}>Register</button>
                 
             </form>
 
