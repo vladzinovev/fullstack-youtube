b01a79bafe1bdf77d8aa74f0168b3bc35eafb4b9
Vladislav <vladozdeveloper@gmail.com>
2022-10-24 22:10:18 +0300

    created auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, HttpCode, Put } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorators';
 import { UserDto } from './user.dto';
 import { UserService } from './user.service';
 
@@ -9,7 +10,9 @@ export class UserController {
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Put('profile')
-  async register(@Body() dto:UserDto){
+  @Auth()
+
+  async updateProfile(@Body() dto:UserDto){
     return this.userService.updateProfile('62665872d79',dto)
   }
 }
