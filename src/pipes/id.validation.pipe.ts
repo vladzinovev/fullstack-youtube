85c01127c28f6e5ada1e478877918f06bdd9c817
Vladislav <vladozdeveloper@gmail.com>
2022-10-25 00:02:23 +0300

    created id validation pipe

---
+++ src/pipes/id.validation.pipe.ts
@@ -0,0 +1,13 @@
+import{ArgumentMetadata,BadRequestException,PipeTransform,} from '@nestjs/common';
+import { Types } from 'mongoose';
+
+export class idvalidationpipe implements PipeTransform{
+    transform(value: string, meta: ArgumentMetadata) {
+        if(meta.type==='param') return value
+
+        if(!Types.ObjectId.isValid(value)){
+            throw new BadRequestException('Invalid format id')
+        }
+        return value
+    }
+}
--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,7 +1,1 @@
-import { Controller } from '@nestjs/common';
-import { VideoService } from './video.service';
 
-@Controller('video')
-export class VideoController {
-  constructor(private readonly videoService: VideoService) {}
-}
