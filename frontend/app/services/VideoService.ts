516be86fa291a7faf77f9dd49eb0d5cfa1bb06ee
Vladislav <vladozdeveloper@gmail.com>
2022-10-28 00:29:29 +0300

    created video service

--- frontend/app/services/VideoService.ts
+++ frontend/app/services/VideoService.ts
@@ -1,3 +1,49 @@
+import { axiosClassic } from "api/interceptors"
+import axios from "axios"
+import { IVideo, IVideoDto } from "types/video.interface"
+
 export const VideoService={
-   
+    async getVideosByUser(userId:string){
+        return axiosClassic.get<IVideo[]>(`/video/by-user/${userId}`)
+    },
+
+    async getVideosByCurrentUser(){
+        return axios.get<IVideo[]>(`/video/by-user-private`)
+    },
+
+    async getVideoById(id:string){
+        return axiosClassic.get<IVideo>(`/video/${id}`)
+    },
+
+    async getAll(id:string){
+        return axiosClassic.get<IVideo[]>(`/video`)
+    },
+
+    async getMostPopular(id:string){
+        return axiosClassic.get<IVideo[]>(`/video/most-popular`)
+    },
+    
+    async getVideoPrivateById(id:string){
+        return axiosClassic.get<IVideo>(`/video/get-private/${id}`)
+    },
+
+    async createVideo(){
+        return axios.post<string>(`/video`)
+    },
+
+    async updateVideo(id:string, body:IVideoDto){
+        return axios.put<IVideo>(`/video/${id}`)
+    },
+
+    async updateViews(id:string){
+        return axios.put<IVideo>(`/video/update-views/${id}`)
+    },
+
+    async updateLikes(id:string){
+        return axios.put<IVideo>(`/video/update-likes/${id}`)
+    },
+
+    async deleteVideo(id:string){
+        return axios.delete(`/video/${id}`)
+    },
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,24 @@ const HomePage: NextPage = () => {
   return <Home/>
 }
 
+export const getStaticProps:GetStaticProps=async()=>{
+  try{
 
+    const {data}
+
+    //weekly featured & new videos & random video
+    //top-video
+    //top-channels
+
+    return{
+      props:{
+
+      },
+      revalidate:60
+    }
+  }catch(e){
+
+  }
+}
 
 export default HomePage;
